b8b6643c1fe8e19c42c538591eedc24f6295b3fb
Basil Mari <basilm4r0@gmail.com>
2023-01-30 00:34:29 +0200

    database improvements

--- library.sql
+++ library.sql
@@ -12,50 +12,44 @@ CREATE TABLE Borrowers (
   Address varchar(32) ,
   PRIMARY KEY (Borrower_ID));
 
-  CREATE TABLE Creators (
-  CID int NOT NULL,
-  PRIMARY KEY (CID));
+CREATE TABLE Creators ( # It turns out this is needed to enforce Creator_ID uniqueness
+  Creator_ID int NOT NULL,
+  PRIMARY KEY (Creator_ID));
 
-
-
-   CREATE TABLE Resources (
+  CREATE TABLE Resources (
   Resource_ID int NOT NULL AUTO_INCREMENT,
   Description varchar(1000) ,
   Title varchar(32) ,
   Creator_ID int NOT NULL,
   PRIMARY KEY (Resource_ID),
-  FOREIGN KEY (Creator_ID) REFERENCES Creators (CID)
-);
-
-
+  FOREIGN KEY (Creator_ID) REFERENCES Creators (Creator_ID));
 
 CREATE TABLE Books (
-  BID int NOT NULL AUTO_INCREMENT,
+  Book_ID int NOT NULL AUTO_INCREMENT,
   Resource_ID int NOT NULL,
   Publisher varchar(32) ,
-  Date_of_publishing date ,
+  Year_of_publication year,
   Language varchar(15),
-  PRIMARY KEY (BID),
+  PRIMARY KEY (Book_ID),
 FOREIGN KEY (Resource_ID) REFERENCES Resources (Resource_ID));
-
 
 CREATE TABLE Authors (
   Author_ID int NOT NULL AUTO_INCREMENT,
-  CID int not NULL,
+  Creator_ID int not NULL,
   Name varchar(32) ,
   Gender varchar(10) ,
   Date_of_birth date ,
   PRIMARY KEY (Author_ID),
-FOREIGN KEY (CID) REFERENCES Creators (CID));
+FOREIGN KEY (Creator_ID) REFERENCES Creators (Creator_ID));
 
 
 CREATE TABLE Collectives (
   Collective_ID int NOT NULL AUTO_INCREMENT,
-  CID int not NULL,
+  Creator_ID int not NULL,
   Name varchar(32) ,
-  Date_of_birth date  ,
+  Date_of_formation date  ,
   PRIMARY KEY (Collective_ID),
-FOREIGN KEY (CID) REFERENCES Creators (CID));
+FOREIGN KEY (Creator_ID) REFERENCES Creators (Creator_ID));
 
 CREATE TABLE Articles (
   AID int NOT NULL AUTO_INCREMENT,
@@ -63,7 +57,7 @@ CREATE TABLE Articles (
   Publisher varchar(32) ,
   Language varchar(15) ,
   Container_text varchar(32) ,
-  Date_of_publishing date ,
+  Date_of_publication date ,
 
   PRIMARY KEY (AID),
 FOREIGN KEY (Resource_ID) REFERENCES Resources (Resource_ID));
@@ -74,7 +68,7 @@ CREATE TABLE Essays (
   Publisher varchar(32) ,
   Language varchar(15) ,
   Container_text varchar(32) ,
-  Date_of_publishing date ,
+  Date_of_publication date ,
 
   PRIMARY KEY (EID),
 FOREIGN KEY (Resource_ID) REFERENCES Resources (Resource_ID));
@@ -119,4 +113,10 @@ FOREIGN KEY (Resource_ID) REFERENCES Resources (Resource_ID));
   FOREIGN KEY (Resource_ID) REFERENCES Resources (Resource_ID),
   PRIMARY KEY (Borrower_ID,Resource_ID,Borrow_date));
 
-insert into Creators values (1);
+INSERT INTO Creators VALUES (1);
+INSERT INTO Authors (Creator_ID, Name, Gender, Date_of_birth)
+VALUES (1, 'Toni Morrison', 'Female', '1931-02-18');
+INSERT INTO Resources (Resource_ID, Description, Title, Creator_ID)
+VALUES (1, 'A novel about a slave', 'Beloved', 1);
+INSERT INTO Books (Book_ID, Resource_ID, Publisher, Year_of_publication, Language)
+Values (1, 1, 'Alfred A. Knopf', '1987', 'English');
